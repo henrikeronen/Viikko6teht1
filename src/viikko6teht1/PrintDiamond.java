ca949f859348cd42ced6ced26a015d2e9fc0a3d3
s718409 <s718409@MIC-B2002-D28.ky.local>
2020-02-12 10:01:46 +0200

    toka versio

--- src/viikko6teht1/PrintDiamond.java
+++ src/viikko6teht1/PrintDiamond.java
@@ -1,34 +1,10 @@
 package viikko6teht1;
-public class PrintDiamond extends Thread {
+public class PrintDiamond {
 
-        private int lkm;
-
-    public PrintDiamond(int lkm) {
-        this.lkm = lkm;
+    public PrintDiamond() {
     }
 
-        
-    @Override
-    public void run(){
-        int counter = 0;
-        while(counter<lkm){
-            System.out.print("*");
-        }    
 
-}
-    
 
-    
-        
-    public int getLkm() {
-        return lkm;
-    }
-
-    public void setLkm(int lkm) {
-        this.lkm = lkm;
-    }
-
-    
-    
     
 }
--- src/viikko6teht1/Viikko6teht1.java
+++ src/viikko6teht1/Viikko6teht1.java
@@ -4,4 +4,5 @@ public class Viikko6teht1 {
   
         
         
+        
     }}
